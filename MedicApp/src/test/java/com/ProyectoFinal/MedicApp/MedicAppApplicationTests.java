382452dae7b4b7f4761530e500c59b33c180aca3
Leandro Hidalgo <104367153+leanmza@users.noreply.github.com>
2023-04-12 23:18:33 -0300

    1° Commit Spring inicializado
    Las dependencias de Spring Security en el archivo pom.xml están comentadas para que no molesten al principio

---
+++ MedicApp/src/test/java/com/ProyectoFinal/MedicApp/MedicAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoFinal.MedicApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
