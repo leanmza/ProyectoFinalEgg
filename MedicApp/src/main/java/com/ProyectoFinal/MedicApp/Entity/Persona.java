c57bc2fd9f2662e48b8ee439f0be4b10ffb28ace
Leandro Hidalgo <104367153+leanmza@users.noreply.github.com>
2023-04-14 19:06:11 -0300

    branch de facundo con correciones buenas prácticas y lombok

---
+++ MedicApp/src/main/java/com/ProyectoFinal/MedicApp/Entity/Persona.java
@@ -0,0 +1,42 @@
+package com.ProyectoFinal.MedicApp.Entity;
+
+import com.ProyectoFinal.MedicApp.Enum.Rol;
+
+import javax.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(
+            strategy = GenerationType.IDENTITY
+    )
+    private Long id;
+    private String nombre;
+    private String apellido;
+    private String email;
+    private String telefono;
+    @Enumerated(EnumType.STRING)
+    private Rol rol;
+    private String foto;
+    private String password;
+    private boolean activo=false;
+
+    public Persona() {
+    }
+
+    public Persona(Long id, String nombre, String apellido, String email, String telefono, Rol rol, String foto, String password, boolean activo) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.email = email;
+        this.telefono = telefono;
+        this.rol = rol;
+        this.foto = foto;
+        this.password = password;
+        this.activo = activo;
+    }
+}
+
+ 
---
+++ MedicApp/src/main/java/com/ProyectoFinal/MedicApp/Service/PersonaService.java
@@ -0,0 +1,58 @@
+package com.ProyectoFinal.MedicApp.Service;
+
+import com.ProyectoFinal.MedicApp.Entity.Persona;
+import com.ProyectoFinal.MedicApp.Exception.MiExcepcion;
+import com.ProyectoFinal.MedicApp.Repository.PersonaRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.transaction.Transactional;
+
+public class PersonaService {
+
+    @Autowired
+    private PersonaRepositorio personaRepositorio;
+
+    @Transactional
+    public void crearPersona(String nombre, String apellido, String email, String telefono, String password) throws MiExcepcion {
+
+        validarPersona(nombre, apellido, email, telefono, password);
+
+        Persona persona = new Persona();
+        persona.setActivo(true);
+        persona.setNombre(nombre);
+        persona.setApellido(apellido);
+        persona.setEmail(email);
+        persona.setTelefono(telefono);
+        persona.setPassword(password);
+
+        personaRepositorio.save(persona);
+
+    }
+
+    public void validarPersona(String nombre, String apellido, String email, String telefono, String password) throws MiExcepcion {
+        try {
+            if (nombre == null || nombre.isEmpty()) {
+                throw new MiExcepcion("El nombre de la persona no puede ser nulo o vacío");
+            }
+
+            if (apellido == null || apellido.isEmpty()) {
+                throw new MiExcepcion("El apellido de la persona no puede ser nulo o vacío");
+            }
+
+            if (email == null || email.isEmpty()) {
+                throw new MiExcepcion("El correo electrónico de la persona no puede ser nulo o vacío");
+            }
+
+            if (telefono == null || telefono.isEmpty()) {
+                throw new MiExcepcion("El teléfono de la persona no puede ser nulo o vacío");
+            }
+
+            if (password == null || password.isEmpty()) {
+                throw new MiExcepcion("La contraseña de la persona no puede ser nula o vacía");
+            }
+
+        } catch (MiExcepcion ex) {
+            throw ex;
+        }
+    }
+}
