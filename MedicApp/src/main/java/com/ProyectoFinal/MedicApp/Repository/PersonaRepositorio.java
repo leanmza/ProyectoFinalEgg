0f878e052f720f55ae35ff63cae0dda58e5e6f25
Facundo <facuang@outlook.com>
2023-04-13 21:32:31 -0300

    Creacion de entity Persona, con su repositorio, servicio, exception y demas...

---
+++ MedicApp/src/main/java/com/ProyectoFinal/MedicApp/Repository/PersonaRepositorio.java
@@ -0,0 +1,9 @@
+package com.ProyectoFinal.MedicApp.Repository;
+
+import com.ProyectoFinal.MedicApp.Entity.Persona;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonaRepositorio extends JpaRepository<Persona, Long> {
+}
