c94101209e262ab743fdb7a971b8bf3613779f7e
Ariel Eberhardt <arielse17@gmail.com>
2023-04-20 19:51:38 -0300

    modificacion en AdministradorControlador

--- MedicApp/src/main/java/com/ProyectoFinal/MedicApp/controller/AdministradorControlador.java
+++ MedicApp/src/main/java/com/ProyectoFinal/MedicApp/controller/AdministradorControlador.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class AdministradorControlador {
     
     @Autowired
-     PacienteService pacienteServicio;
+    PacienteService pacienteServicio;
     
     @Autowired
     ProfesionalService profesionalServicio;
