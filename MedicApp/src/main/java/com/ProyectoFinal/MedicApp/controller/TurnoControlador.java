0f76c47e6e265fb2dc3568edd22c2c9d1f93650f
Ariel Eberhardt <arielse17@gmail.com>
2023-05-05 11:08:14 -0300

    Inyeccion de Profesional en plantilla de Turno

--- MedicApp/src/main/java/com/ProyectoFinal/MedicApp/controller/TurnoControlador.java
+++ MedicApp/src/main/java/com/ProyectoFinal/MedicApp/controller/TurnoControlador.java
@@ -4,7 +4,6 @@
  */
 package com.ProyectoFinal.MedicApp.controller;
 
-
 import com.ProyectoFinal.MedicApp.Entity.Profesional;
 import com.ProyectoFinal.MedicApp.Exception.MiExcepcion;
 import com.ProyectoFinal.MedicApp.Repository.ProfesionalRepositorio;
@@ -16,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,50 +24,47 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  * @author Lean
  */
-
 @Controller
 @RequestMapping("/turno")
 public class TurnoControlador {
-    
+
     @Autowired
     ProfesionalRepositorio profesionalRepositorio;
-    
-   @GetMapping("/formularioTurno")
-   public String turno(@RequestParam String idProfesional,Model model){
 
-    Optional<Profesional> profesional = profesionalRepositorio.findById(idProfesional); //SEGUN EL CHAT ESTO DEBERÍA ANDAR PERO ME TIRÓ UN 404 ASIQUE ESTA CERCA DE ANDAR
-    model.addAttribute("profesional", profesional);
-       System.out.println("profesional" + profesional);
-       
-   return "formulario_turno.html";
-    }
-   
-   
-   @Transactional
-    @PostMapping("/registroTurno")
-    public String registroTurno(@RequestParam String especialidad, @RequestParam String nombre, @RequestParam String apellido ) throws MiExcepcion{
-       
-////        try {
-////            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
-////            Date fechaNacimiento = formato.parse(nacimiento);
-////            
-////    
-////
-////            System.out.println("Turno exitoso");
-////            return "redirect:/inicio";
-//
-//        } catch (MiExcepcion me) {
-//            System.out.println("Registro de turno FALLIDO!\n" + me.getMessage());
-//
-//            return "formulario_paciente.html";
-//
-//        } catch (ParseException ex) {
-//            System.out.println(ex.getMessage());
-//            ex.printStackTrace();
-            return "inicio.html";
+    @GetMapping("/formularioTurno/{idProfesional}")
+    public String turno(@PathVariable String idProfesional, Model model) {
+        
+        Optional<Profesional> respuesta = profesionalRepositorio.findById(idProfesional);
+        if (respuesta.isPresent()) {
+            Profesional profesional = respuesta.get();
+            model.addAttribute("profesional", profesional);
+            System.out.println("profesional" + profesional);
         }
+        
+        return "formulario_turno.html";
     }
-   
-   
-   
 
+    @Transactional
+    @PostMapping("/registroTurno")
+    public String registroTurno(@RequestParam(required = false) String especialidad, @RequestParam(required = false) String nombre, @RequestParam(required = false) String apellido) throws MiExcepcion {
+
+        try {
+            SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+            Date fechaNacimiento = formato.parse(nacimiento);
+            
+    
+
+            System.out.println("Turno exitoso");
+            return "redirect:/inicio";
+
+        } catch (MiExcepcion me) {
+            System.out.println("Registro de turno FALLIDO!\n" + me.getMessage());
+
+            return "formulario_paciente.html";
+
+        } catch (ParseException ex) {
+            System.out.println(ex.getMessage());
+            ex.printStackTrace();
+        return "inicio.html";
+    }
+}
